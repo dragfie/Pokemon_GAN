8f757712d7e6bf8cd77affcc97345c0ea1961942
dragfie <brandelmartin@gmail.com>
2018-12-14 18:49:07 +1100

    adding resized images

--- RGBA2RGB.py
+++ RGBA2RGB.py
@@ -10,7 +10,8 @@ for each in os.listdir(src):
         png.load() # required for png.split()
         background = Image.new("RGB", png.size, (0,0,0))
         background.paste(png, mask=png.split()[3]) # 3 is the alpha channel
+        background.convert('RGB')
         background.save(os.path.join(dst,each.split('.')[0] + '.jpg'), 'JPEG')
     else:
-        png.convert('RGB')
+        png = png.convert('RGB')
         png.save(os.path.join(dst,each.split('.')[0] + '.jpg'), 'JPEG')
